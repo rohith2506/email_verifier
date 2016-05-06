61ecf69d026d462905f23c1f19bee86afa57da62
Rohith Uppala <rohith.uppala@goibibo.com>
2016-05-06 17:08:07 +0530

    first commit for email verifier

---
+++ __init__.py
@@ -0,0 +1,6 @@
+Gibberish Email Verifier
+
+Project Scope: Given an email, it returns whether that email is gibberish or fake with confidence level
+Programming language: Python
+
+Author: Rohith Uppala
